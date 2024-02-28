986c113d1b4c5fd8d40ebb687371676aeb0374e7
Nikita <chesterryos@gmail.com>
2024-02-29 07:03:29 +0900

    Изменили способ вывода всех товаров в products.html, вывели все товары в виде таблицы, добавили вывод времени, добавили метод вывода времени во views.py

--- project/product/views.py
+++ project/product/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from datetime import datetime
 from django.views.generic import ListView, DetailView
 from .models import Product
 
@@ -15,6 +16,21 @@ class ProductsList(ListView):
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'products'
 
+    # Метод get_context_data позволяет нам изменить набор данных,
+    # который будет передан в шаблон.
+    def get_context_data(self, **kwargs):
+        # С помощью super() мы обращаемся к родительским классам
+        # и вызываем у них метод get_context_data с теми же аргументами,
+        # что и были переданы нам.
+        # В ответе мы должны получить словарь.
+        context = super().get_context_data(**kwargs)
+        # К словарю добавим текущую дату в ключ 'time_now'.
+        context['time_now'] = datetime.utcnow()
+        # Добавим ещё одну пустую переменную,
+        # чтобы на её примере рассмотреть работу ещё одного фильтра.
+        context['next_sale'] = "Распродажа в среду!"
+        return context
+
 class ProductDetail(DetailView):
     # Модель всё та же, но мы хотим получать информацию по отдельному товару
     model = Product
@@ -22,3 +38,4 @@ class ProductDetail(DetailView):
     template_name = 'product.html'
     # Название объекта, в котором будет выбранный пользователем продукт
     context_object_name = 'product'
+
